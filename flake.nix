debf0acb0647d1f15a4e339603012e440b1dac04
Vivian <vivian@0x76.dev>
2024-01-04 10:59:15 +0100

    add staging

--- flake.nix
+++ flake.nix
@@ -9,27 +9,29 @@
         pkgs = import nixpkgs { inherit system; };
         nativeBuildInputs = with pkgs; [ zola ];
         buildInputs = with pkgs; [ ];
-        nerdfonts = pkgs.nerdfonts.override {
-          fonts = [ "NerdFontsSymbolsOnly" ];
-        };
-        
-      in
-      rec {
+        nerdfonts =
+          pkgs.nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; };
+
+      in rec {
         packages = rec {
           website = pkgs.stdenv.mkDerivation {
             pname = "essentials-website";
-            version = "2023-12-27";
+            version = "2024-01-04";
             src = self;
             inherit nativeBuildInputs buildInputs;
             configurePhase = ''
               # Pre-processing here
               mkdir -p ./static/fonts
+              echo "${self.rev or "dirty"}" > static/rev
               ln -sf ${nerdfonts}/share/fonts/truetype/NerdFonts/* static/fonts/
               ln -sf ${pkgs.fira}/share/fonts/opentype/* static/fonts/
               ln -sf ${pkgs.fira-mono}/share/fonts/opentype/* static/fonts/
             '';
             buildPhase = "zola build";
             installPhase = "cp -r public $out";
+          };
+          staging = website.overrideAttrs {
+            buildPhase = "zola --config config.staging.toml build";
           };
           default = website;
         };
